a5bfdfe16441ad5fb1068d7f0456dc73ffe99792
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-26 15:59:03 +0200

    :tada: add setup  :tada:

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Home from './components/Home/Home'
 
 const HomePage = () => {
   return (
-    <div className='text-7xl text-red-600'>HomePage</div>
+    <div className='overflow-hidden'>
+      <Home/>
+    </div>
   )
 }
 
