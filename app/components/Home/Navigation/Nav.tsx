c93a32e65848ebc4e6865f1dcb7110c81d02edc9
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-29 00:32:29 +0200

    :tada: working on the nav  :tada:

--- app/components/Home/Navigation/Nav.tsx
+++ app/components/Home/Navigation/Nav.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='fixed w-full transition-all duration-200 z-[1000]'>
+        <div className='flex items-center h-[10vh] lg:h-[12vh] justify-between w-4/5 mx-auto'>
+            {/* Logo */}
+            <div className='font-bold text-white text-2xl'>
+                <span className='text-3xl md:text-4xl font-bold text-yellow-300'>We</span>
+                Host
+            </div>
+        </div>
+    </div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import ResponsiveNav from "./components/Home/Navigation/ResponsiveNav";
 
 const font = Plus_Jakarta_Sans({
   weight:["200", "300", "400", "500", "600", "700", "800"],
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ResponsiveNav/>
+          {children}
+      </body>
     </html>
   );
 }
