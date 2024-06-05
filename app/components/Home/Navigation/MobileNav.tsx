209706c3fd8a70e90b0e895425582d8b40cb3569
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-06-05 09:35:40 +0200

    :tada: nav mobile  :tada:

--- app/components/Home/Navigation/MobileNav.tsx
+++ app/components/Home/Navigation/MobileNav.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <div>
+      <div className='fixed top-0 left-0 right-0 bottom-0 transform transition-all duration-500 z-[1000]
+      bg-black opacity-70 h-screen'></div>
+    </div>
   )
 }
 
