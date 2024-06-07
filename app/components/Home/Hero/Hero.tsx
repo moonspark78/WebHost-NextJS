4e848f7853fdf27ba42927918608d4267d2d8127
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-06-07 17:38:36 +0200

    :tada: hero section  :tada:

--- app/components/Home/Hero/Hero.tsx
+++ app/components/Home/Hero/Hero.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { CiDiscount1 } from 'react-icons/ci'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='w-full pt-[4vh] md:pt-[12vh] h-screen bg-blue-700'>
+      <div className='flex justify-center flex-col w-4/5 h-full mx-auto'>
+        {/* define grid */}
+        <div className='w-full mx-auto grid grid-cols-1 lg:grid-cols-2 items-center gap-12'>
+          {/* Text Content */}
+          <div>
+            <div className='flex items-center space-x-3 px-6 py-2 w-fit rounded-full border-2 border-gray-300'>
+              <CiDiscount1 className='h-6 w-6 text-yellow-300'/>
+              <span className='text-xs md:text-lg text-white font-semibold text-opacity-70'>
+                30% Discount First Month purchaes
+              </span>
+            </div>
+            <h1 className='text-3xl md:text-5xl lg:text-[55px] xl:text-[65px] relative z-[1] md:leading-[4.5rem] leading-[3rem]
+             mt-4 text-white font-bold'>
+              Premium Hosting Technogies
+              <br />
+              <span className='absolute bottom-4 '></span>
+            </h1>
+          </div>
+          {/* Image Content */}
+          <div></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from './Hero/Hero'
 
 const Home = () => {
   return (
-    <div></div>
+    <div>
+      <Hero/>
+    </div>
   )
 }
 
