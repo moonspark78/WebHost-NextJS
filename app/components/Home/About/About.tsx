20256b0289e2bb4b9c2a747e56eaf7e4afd7beb1
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-06-08 02:41:40 +0200

    :tada: ...  :tada:

--- app/components/Home/About/About.tsx
+++ app/components/Home/About/About.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import SectionHeading from '../../Helper/SectionHeading'
 
 const About = () => {
   return (
     <div className='pt-20 pb-16'>
         {/* Section Heading */}
+        <SectionHeading heading='About Us'/>
+        {/* Define grid Section */}
+        <div className='w-4/5 mx-auto items-center mt-24 grid grid-cols-1 lg:grid-cols-2 gap-12'>
+            {/* Image Content */}
+            <div></div>
+            {/* Text Content */}
+            <div></div>
+        </div>
     </div>
   )
 }
