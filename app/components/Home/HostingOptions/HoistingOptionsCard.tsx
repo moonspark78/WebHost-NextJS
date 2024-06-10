bc3a07907e69d239a96c8adb8bcb0eb4949c021d
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-06-10 15:21:21 +0200

    :tada: hostingOptions Card  section  :tada:

--- app/components/Home/HostingOptions/HoistingOptionsCard.tsx
+++ app/components/Home/HostingOptions/HoistingOptionsCard.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import React from 'react'
+import { BiArrowToRight } from 'react-icons/bi';
 import Tilt from 'react-parallax-tilt';
 
 type Props={
@@ -24,6 +25,16 @@ const HoistingOptionsCard = ({icon,title,price}:Props) => {
                     className='mx-auto mt-8 mb-8'
                 />
             </div>
+            <h1 className='text-center text-[#232c45] font-semibold capitalize text-[18px]'>
+                {title}
+            </h1>
+            <p className='text-center text-black text-opacity-70 text-[14px] mt-4 mb-4'>Lorem ipsum dolor sit amet consectetur.</p>
+            <p className='text-[#232c45] text-center font-semibold text-[14px]'>Starting from  ${price}/month {" "}</p>
+            <button className='px-6 py-2 font-semibold text-blue-950 hover:bg-blue-700 hover:text-white transition-all
+             duration-300 mx-auto border-[1.2px] border-gray-400 border-opacity-25 mt-8 flex items-center space-x-2 rounded-lg '>
+                <span>See Plan</span>
+                <BiArrowToRight/>
+            </button>
         </div>
     </Tilt>
   )
