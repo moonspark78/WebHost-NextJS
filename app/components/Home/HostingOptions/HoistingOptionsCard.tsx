7a1fc83359b09ad36965cfc3a1bd788b6234a2d3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-06-10 15:00:35 +0200

    :tada: hostingOptions Cards  section  :tada:

--- app/components/Home/HostingOptions/HoistingOptionsCard.tsx
+++ app/components/Home/HostingOptions/HoistingOptionsCard.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+"use client"
 
-const HoistingOptionsCard = () => {
+import Image from 'next/image';
+import React from 'react'
+import Tilt from 'react-parallax-tilt';
+
+type Props={
+    icon: string;
+    title: string;
+    price: string; 
+}
+
+const HoistingOptionsCard = ({icon,title,price}:Props) => {
   return (
-    <div>HoistingOptionsCard</div>
+    <Tilt className='parallax-effect' perspective={500}>
+        <div className='bg-gray-200 rounded-lg h-full p-6 w-full'>
+            {/* Image Icon */}
+            <div>
+                <Image
+                    alt={title}
+                    src={icon}
+                    width={80}
+                    height={80}
+                    className='mx-auto mt-8 mb-8'
+                />
+            </div>
+        </div>
+    </Tilt>
   )
 }
 
