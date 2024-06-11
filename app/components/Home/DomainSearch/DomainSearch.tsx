a4bfb62cf88c1b1ef929f380d39384affd9a32bf
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-06-11 09:13:06 +0200

    :tada: create DomainSearch  :tada:

--- app/components/Home/DomainSearch/DomainSearch.tsx
+++ app/components/Home/DomainSearch/DomainSearch.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const DomainSearch = () => {
   return (
     <div className='pt-12 md:pt-20 pb-16 '>
-      <div className='w-[90%] p-6 rounded-2xl bg-blue-700 mx-auto '></div>
+      <div className='w-[90%] p-6 rounded-2xl bg-blue-700 mx-auto'>
+        {/* Heading */}
+        <h1 className='text-center mt-8 text-white font-semibold text-xl sm:text-2xl lg:text-3xl tracking-wider'>
+          A Domain That Stands Tall on a Billboard
+        </h1>
+      </div>
     </div>
   )
 }
