14f957d5a77068928660b6683e79538cd293bedf
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-06-21 12:50:18 +0200

    :tada: create first pricecard  :tada:

--- app/components/Home/Price/Price.tsx
+++ app/components/Home/Price/Price.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SectionHeading from '../../Helper/SectionHeading'
+import PriceCard from './PriceCard'
 
 const Price = () => {
   return (
@@ -10,7 +11,7 @@ const Price = () => {
         <div className='w-4/5 mx-auto mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 items-center'>
         {/* Price Card 1 */}
             <div>
-                
+                <PriceCard image="/images/p1.png" plan="Basic" price="9.99"/>
             </div>
         </div>
     </div>
--- app/components/Home/Price/PriceCard.tsx
+++ app/components/Home/Price/PriceCard.tsx
@@ -1,8 +1,30 @@
+import Image from 'next/image';
 import React from 'react'
 
-const PriceCard = () => {
+
+type Props ={
+  image:string;
+  plan:string;
+  price:string;
+}
+
+
+
+const PriceCard = ({image, plan, price}:Props) => {
   return (
-    <div>PriceCard</div>
+    <div className='bg-[#e3f0ff] hover:scale-110 transition-all duration-500 rounded-lg p-4 md:p-10'>
+      {/* Image Icon */}
+      <Image
+        src={image}
+        alt={plan}
+        width={50}
+        height={50}
+        className='mt-8 mb-8'
+      />
+      <h1 className='text-2xl font-bold tracking-wider text-blue-950'>
+        {plan}
+      </h1>
+    </div>
   )
 }
 
