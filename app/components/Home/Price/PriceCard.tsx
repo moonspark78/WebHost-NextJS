d03ec7120e87e04ca8c96c78332c15eb6ca3bbc8
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-06-21 13:52:25 +0200

    :tada: addind description  :tada:

--- app/components/Home/Price/PriceCard.tsx
+++ app/components/Home/Price/PriceCard.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import React from 'react'
+import { FaCheck } from 'react-icons/fa';
+import Button from '../../Helper/Button';
 
 
 type Props ={
@@ -24,6 +26,35 @@ const PriceCard = ({image, plan, price}:Props) => {
       <h1 className='text-2xl font-bold tracking-wider text-blue-950'>
         {plan}
       </h1>
+      <p className='mt-2 text-black text-opacity-60 text-sm font-medium'>
+        Added Privacy and security features
+      </p>
+      <div className='mt-8 mb-8 h-[1.5px] opacity-20 rounded-lg bg-gray-500 mx-auto'/>
+      <div className='flex items-center mb-8 space-x-2'>
+          <FaCheck className='text-green-600'/>
+          <p className='text-xs font-bold text-blue-950'>
+            Free Domain (${price} value)
+          </p>
+      </div>
+      <div className='flex items-center mb-8 space-x-2'>
+          <FaCheck className='text-green-600'/>
+          <p className='text-xs font-bold text-blue-950'>
+            Regular Security Patching
+          </p>
+      </div>
+      <div className='flex items-center mb-8 space-x-2'>
+          <FaCheck className='text-green-600'/>
+          <p className='text-xs font-bold text-blue-950'>
+            HTTP /2 Enabled Servers
+          </p>
+      </div>
+      <div className='flex items-center mb-8 space-x-2'>
+          <FaCheck className='text-green-600'/>
+          <p className='text-xs font-bold text-blue-950'>
+            Free Object Chche Pro
+          </p>
+      </div>
+      <Button bg="bg-rose-600">Get Started</Button>
     </div>
   )
 }
